596422f0b67412bcc52023d1eab29527138d8248
Eric Pershey <pershey@anl.gov>
2019-12-19 21:39:43 +0000

    updating image

--- docker/centos7_ptl/build.sh
+++ docker/centos7_ptl/build.sh
@@ -6,7 +6,3 @@ cd /src/pbspro
 ./autogen.sh
 ./configure --prefix=$PBS_INSTALL_DIR --enable-ptl
 make -j 7
-sudo make install
-sudo /opt/pbs/libexec/pbs_postinstall
-sudo chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
-sudo /opt/pbs/unsupported/pbs_config --make-ug
---
+++ docker/centos7_ptl/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make install
+/opt/pbs/libexec/pbs_postinstall
+chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
+/opt/pbs/unsupported/pbs_config --make-ug
