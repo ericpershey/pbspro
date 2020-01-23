990c9d56ddba7b77ee66dd6ba3e90bf65231c37b
Eric Pershey <pershey@anl.gov>
2020-01-23 23:01:10 +0000

    updating the image

--- k8s/build_images.sh
+++ k8s/build_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build . -f Dockerfile.base -t pbspro/pbspro_base_202001:centos7
-docker build --build-arg PBSPRO_GITHUB_PATH=ericpershey . -f Dockerfile.server -t pbspro/pbspro_server_202001:centos7 --force-rm --no-cache
-docker build --build-arg PBSPRO_GITHUB_PATH=ericpershey . -f Dockerfile.node -t pbspro/pbspro_node_202001:centos7 --force-rm --no-cache
+docker build --build-arg PBSPRO_GITHUB_PATH=ericpershey . -f Dockerfile.server -t pbspro/pbspro_server_202001:centos7 --force-rm
+docker build --build-arg PBSPRO_GITHUB_PATH=ericpershey . -f Dockerfile.node -t pbspro/pbspro_node_202001:centos7 --force-rm
+# --no-cache
