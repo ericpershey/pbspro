584104e5845921ef48452eb56d189ee3c529bf97
Eric Pershey <pershey@anl.gov>
2020-02-12 22:02:04 +0000

    fixing image for debian9

--- docker/debian9/build_images.sh
+++ docker/debian9/build_images.sh
@@ -2,6 +2,7 @@
 export PBS_IMAGE_DATE=202002
 export PBS_IMAGE_TAG=debian9
 docker build . -f Dockerfile.base -t pbspro/pbspro_base_$PBS_IMAGE_DATE:$PBS_IMAGE_TAG --force-rm --no-cache
+# note all changes to the repo must be pushed to get these built.
 docker build --build-arg PBSPRO_GITHUB_PATH=ericpershey . -f Dockerfile.pbs -t pbspro/pbspro_build_$PBS_IMAGE_DATE:$PBS_IMAGE_TAG --force-rm --no-cache
 cp -p /usr/bin/docker-init .
 docker build  . -f Dockerfile.server -t pbspro/pbspro_server_$PBS_IMAGE_DATE:$PBS_IMAGE_TAG --force-rm --no-cache
--- docker/debian9/build_pbspro.sh
+++ docker/debian9/build_pbspro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/pbsuser/pbspro
+echo `pwd`
+ls
 export PBS_INSTALL_DIR=/opt/pbs
 ./autogen.sh
 ./configure --prefix=$PBS_INSTALL_DIR --enable-ptl
